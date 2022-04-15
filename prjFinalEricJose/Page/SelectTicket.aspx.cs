fa4d8235e293ec1d110caaf6a9e709cb8cfc647f
jose125CR <ol125@hotmail.es>
2022-04-14 18:31:34 -0600

    antes de instalar el nugget

--- prjFinalEricJose/Page/SelectTicket.aspx.cs
+++ prjFinalEricJose/Page/SelectTicket.aspx.cs
@@ -11,7 +11,6 @@ namespace prjFinalEricJose.Page
     {
         public void Mensaje(string pMensaje)
         {
-            //HttpUtility.ParseQueryString(myU)
             Type cstype = this.GetType();
 
             ClientScriptManager cs = Page.ClientScript;
@@ -25,24 +24,38 @@ namespace prjFinalEricJose.Page
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (IsPostBack == false)
+            for (int i = 1; i <= 50; i++)
             {
-                for (int i = 1; i <= 10; i++)
-                {
-                    Button btn = new Button();
-                    btn.CssClass = "seat me-1 ms-1";
-                    btn.Text = "A-" + i.ToString();
-                    btn.Click += (se, ev) => button_Click(se, ev, 10);
-                    pnl_butacas.Controls.Add(btn);
-                }
+                Button btn = new Button();
+                btn.CssClass = "seat me-1 ms-1 mt-2" + ConseguirClaseButaca(i);
+                btn.Text = "A-" + i.ToString();
+                btn.Click += (se, ev) => button_Click(se, ev, btn.Text);
+                pnl_butacas.Controls.Add(btn);
             }
             //int id = Convert.ToInt32(ViewState["id_pelicula"]);
             //Mensaje(Convert.ToString(Session["id_pelicula"]));
         }
 
-        public void button_Click(object sender, EventArgs e, int pa)
+        private string ConseguirClaseButaca(int pa)
         {
-            Mensaje(pa.ToString());
+            if (pa % 5 == 0)
+            {
+                return " ms-5";
+            }
+            else if ((pa - 1) % 5 == 0)
+            {
+                System.Diagnostics.Debug.WriteLine(pa, "Yo soy pa");
+                return " me-5";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void button_Click(object sender, EventArgs e, string pa)
+        {
+            Mensaje(pa);
         }
     }
 }
